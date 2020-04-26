2b1090ac9bdcac70b6f957eab13a1a22b67dc804
chenyanming <elecming@gmail.com>
2020-04-26 22:33:01 +0800

    Fix compile warning.

--- shrface.el
+++ shrface.el
@@ -50,7 +50,7 @@
 (require 'outline)
 (require 'org-indent)
 (require 'compile)
-;; (require 'pcre2el)
+(require 'pcre2el)
 
 (ignore-errors
   ;; in case the users lazy load org-mode before require shrface
