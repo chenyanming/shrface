83d9397dbee5ee10113d2258c70231ff16dce8a5
chenyanming <elecming@gmail.com>
2020-04-26 12:38:46 +0800

    Update commentary.

--- shrface.el
+++ shrface.el
@@ -24,8 +24,24 @@
 
 ;;; Commentary:
 
-;; It is a shr faces package that helps you apply your favourite org features to
-;; shr/eww. Besides, it provides you the anaysis tools.
+;; This package extends `shr' / `eww' with org features and analysis capability.
+;; It can be used in `dash-docs', `eww', `nov.el', and `mu4e'.
+;; - Configurable org-like heading faces, headline bullets, item bullets, paragraph
+;;   indentation, fill-column, item bullet, versatile hyper
+;;   links(http/https/file/mailto/etc) face and so on.
+;; - Browse the internet or local html file with `eww' just like org mode.
+;; - Read dash docsets with `dash-docs' and the beauty of org faces.
+;; - Read epub files with `nov.el' , just like org mode.
+;; - Read html email with `mu4e' , the same reading experience just like org mode
+;;   without formatting html to org file.
+;; - Switch/jump the headlines just like org-mode in `eww' and `nov.el' with `imenu'
+;; - Togle/cycle the headlines just like org-mode in `eww' and `nov.el' with `outline-minor-mode'
+;;   and `org-cycle'/`org-shifttab'
+;; - Enable indentation just like org-mode in `eww' and `nov.el' with `org-indent-mode'
+;; - Analysis capability for Researchers:
+;;   - Headline analysis: List all headlines with clickable texts.
+;;   - URL analysis: List all classified URL with clickable texts.
+
 
 ;;; Code:
 
