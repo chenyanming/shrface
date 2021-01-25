89e9354fbd69635d7ad6c32a43736af4d2e59650
chenyanming <elecming@gmail.com>
2021-01-26 02:29:34 +0800

    Disable data-src, and find another solution.

--- shrface.el
+++ shrface.el
@@ -745,8 +745,7 @@ Argument DOM dom."
 (defun shrface-insert-org-link (url dom)
   "TODO: Insert org link based on URL and DOM."
   (let* ((img-dom (dom-by-tag dom 'img))
-         (img-src (or (dom-attr img-dom 'data-src) ; https://manga1000.com/ need it
-                      (dom-attr img-dom 'src)
+         (img-src (or (dom-attr img-dom 'src)
                       (let ((srcset (or (dom-attr img-dom 'srcset)
                                         (dom-attr img-dom 'data-srcset))))
                         (if (stringp srcset)
